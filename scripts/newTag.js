cfadcb75d3be6eaa8481b05e5f5f0b30f83ca0dd
AnnAngela <naganjue@vip.qq.com>
2024-01-15 16:04:17 +0800

    ci: fix bug

--- scripts/newTag.js
+++ scripts/newTag.js
@@ -1,6 +1,7 @@
 import { createInterface } from "node:readline";
-import { valid, gt } from "semver";
 import path from "node:path";
+import { valid, gt } from "semver";
+import { resolvePackageJSON } from "pkg-types";
 import execCommand from "./modules/spawnChildProcess.js";
 
 const pkg = process.env.npm_package_name;
@@ -38,8 +39,10 @@ if (tagList.includes(tag)) {
 console.log(`tag: ${tag}`);
 
 console.log("Bump the package version");
-await execCommand(`npm version ${tag.replace(/^v/, "")} -m "release: ${pkg}@v%s"`, { synchronousStderr: true, synchronousStdout: true });
-await execCommand(`git tag -s -m "release: ${pkg}@v${tag}" ${pkg}@v${tag}`, { synchronousStderr: true, synchronousStdout: true, cwd: rootDir });
+await execCommand(`npm version ${tag.replace(/^v/, "")}`, { synchronousStderr: true, synchronousStdout: true });
+await execCommand(`git add ${path.relative(rootDir, await resolvePackageJSON())}`, { synchronousStderr: true, synchronousStdout: true, cwd: rootDir });
+await execCommand(`git commit -S -m "release: ${pkg}@${tag}" -- ${path.relative(rootDir, await resolvePackageJSON())}`, { synchronousStderr: true, synchronousStdout: true, cwd: rootDir });
+await execCommand(`git tag -s -m "release: ${pkg}@${tag}" ${pkg}@${tag}`, { synchronousStderr: true, synchronousStdout: true, cwd: rootDir });
 
 console.log("Pushing...");
 await execCommand("git push --follow-tags", { synchronousStderr: true, synchronousStdout: true, cwd: rootDir });
@@ -50,9 +53,9 @@ draftReleaseURL.pathname += "/releases/new";
 console.log("Draft release URL:", draftReleaseURL.toString());
 console.log("Release title:", `${pkg}@${tag}`);
 await execCommand(`git log --reverse --pretty=format:"* %s (%h)" ${pkg}@v${oldTag}...${pkg}@${tag}`).then((changelog) => {
+    console.log("-".repeat(73));
     console.log("Example changelog:");
     console.info(changelog);
-    console.log("-".repeat(73));
+    console.info("");
 }).catch(() => void 0);
-console.info("");
 console.log("-".repeat(73));
