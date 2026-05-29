06aeeaea4c484382bab87a5fa53070ef63a95d72
AnnAngela <naganjue@vip.qq.com>
2026-05-29 18:16:01 +0800

    chore: remove unused scripts and dependencies
    Delete historical residual scripts that are not part of the main build
    chain: scripts/build.js and 9 modules in scripts/modules/ that were
    only used by the deleted root build script. Remove unused simple-git
    dependency.

--- scripts/modules/IS_IN_GITHUB_ACTIONS.js
+++
@@ -1,2 +0,0 @@
-export const IS_IN_GITHUB_ACTIONS = process.env.GITHUB_ACTIONS === "true";
-export default IS_IN_GITHUB_ACTIONS;
--- scripts/modules/fixZero.js
+++
@@ -1,5 +0,0 @@
-/**
- * @param { number | string } n base string
- * @param { number } [l=2] minimum length, default is `2`
- */
-export default (n, l = 2) => `${n}`.padStart(l, "0");
